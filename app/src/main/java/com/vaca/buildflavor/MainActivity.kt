19f5ba5460e9b3128bb2b66fc6ad4afbdbb28d2c
liguanghao <liguanghao@viatomtech.com>
2022-03-28 09:27:54 +0800

    Initial commit

---
+++ app/src/main/java/com/vaca/buildflavor/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.vaca.buildflavor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val a= BuildConfig.FLAVOR
+        Log.e("fuck",a)
+    }
+}
